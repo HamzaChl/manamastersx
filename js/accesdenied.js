dd05a24ad527daa9723c8c2087c6a91eff32d5fd
HamzaChl <hamza.rotf@gmail.com>
2024-02-08 01:54:04 +0100

    popup

--- js/accesdenied.js
+++ js/accesdenied.js
@@ -1,8 +1,27 @@
-const accesDenied = document.getElementsByClassName("accesDenied");
+// const accesDenied = document.getElementsByClassName("accesDenied");
 
-for (const project of accesDenied) {
-    project.addEventListener("click", function (e) {
-        alert("Toegang geweigerd");
-    })
-}
+// for (const project of accesDenied) {
+//     project.addEventListener("click", function (e) {
+//         alert("Toegang geweigerd");
+//     })
+// }
 
+// Correctie voor nieuwe layout
+
+document.addEventListener("DOMContentLoaded", function () {
+  const projectHolders = document.querySelectorAll(
+    ".project-holder:not(.accepted)"
+  );
+
+  projectHolders.forEach(function (projectHolder) {
+    projectHolder.addEventListener("click", function () {
+      document.getElementById("popup").style.visibility = "visible";
+      document.getElementById("popup").style.opacity = "1";
+    });
+  });
+
+  document.getElementById("close").addEventListener("click", function () {
+    document.getElementById("popup").style.visibility = "hidden";
+    document.getElementById("popup").style.opacity = "0";
+  });
+});
